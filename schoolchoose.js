0a122f02c3972be16d57ac562c8a12c7f28bc710
Por5as <por5as007@gmail.com>
2024-10-30 19:08:35 +0100

    Added schoolchoose search with url encoding and session storage

--- schoolchoose.js
+++ schoolchoose.js
@@ -16,8 +16,52 @@ async function GetSchoolList() {
 
 GetSchoolList()
 
-function DisplayData(data) {
-    for (let i = 1; i < data.length; i++ ){
-        document.write(data[i]["name"] + "<br/>")
-    }
+
+function CityClick(element){
+
+  let name = element.innerText
+  if (name.includes(".")) {
+    name = encodeURIComponent(name.split(".", 1))
+  }
+  else {
+    name = encodeURIComponent(name)
+  }
+  
+  window.sessionStorage.setItem("city", name)
 }
+
+function DisplayData(data) {
+    let list = document.getElementById("list")
+    for (let i = 1; i < data.length; i++ ){
+        let city = document.createElement("li")
+        let link = document.createElement("a")
+        link.href = "./getschoolurl.html"
+        let name = data[i]["name"]
+        city.id = "city"
+        link.innerHTML = name
+        list.appendChild(city)
+        city.appendChild(link)
+        link.setAttribute("onclick", "CityClick(this)");
+    }
+
+}
+function Search() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('input');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("list");
+    li = ul.getElementsByTagName("li");
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
+
